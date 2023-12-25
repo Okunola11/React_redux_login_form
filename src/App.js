84e057d5d965c251a6c8e51b95bfe9e1cf82668a
Okunola Abdulwasiu <abdayoade@gmail.com>
2023-12-25 14:58:22 +0100

    Added App.js routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import Welcome from "./features/auth/Welcome";
+import RequireAuth from "./features/auth/RequireAuth";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Public Routes */}
+        <Route index element={Public} />
+        <Route path="login" element={Login} />
+
+        {/* Protected Routes */}
+        <Route element={RequireAuth}>
+          <Route path="welcome" element={Welcome} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
